3fb8a81a1fff8373bd01a52403eeb9b7b8e754ee
Ab4rdo <fryderyk97@gmail.com>
2021-06-10 20:40:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<h1>Jebać PIS</h1><br>
-	<img src="https://media1.tenor.com/images/fe51db8009bb6bd4d2480ef6afe66f35/tenor.gif?itemid=7909153">'));
+app.get('/', (req, res) => res.send('Jebać PIS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
